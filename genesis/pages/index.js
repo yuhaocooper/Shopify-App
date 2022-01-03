a3300b5a2afa3db508fcbb17075094810fbe9d37
yuhao <tanyuhao@hotmail.com>
2022-01-03 23:22:14 +0800

    initial tutorial code

--- genesis/pages/index.js
+++ genesis/pages/index.js
@@ -1,9 +1,34 @@
-import { Heading, Page, Button } from "@shopify/polaris";
+import { Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
+import React from "react";
 
-const Index = () => (
-  <Page>
-    <Heading>Hello World</Heading>
-  </Page>
-);
+
+class Index extends React.Component{
+  state = { open: false}
+  render(){
+    return (
+      <Page
+      title='Product selector'
+      primaryAction={{
+        content: 'Select products',
+        onAction: () => this.setState({open: true})
+      }}
+    >
+      <ResourcePicker
+        resourceType='Product'
+        open={this.state.open}
+        onCancel={() => this.setState({open: false})}
+        onSelection= { (resources) => this.handleSelection(resources)}
+      />
+    </Page>
+    )
+  }
+  handleSelection = (resources) => {
+    const idFromResources = resources.selection.map((product => product.id))
+    this.setState({open: false})
+    console.log(idFromResources)
+  }
+}
+
 
 export default Index;
